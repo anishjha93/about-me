9ddd590b8b38a8d8bd254addbbb23cff3f196ee8
Anish Kumar Jha <anish.jha@flipkart.com>
2019-11-10 21:09:52 +0530

    Updates

---
+++ precache-manifest.ce3c99fd136b8084ac01ead0cb62a6d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cded2ae3e06f3dfd73268a1b3c2a3b0a",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "56a96feea4625ae615a6",
+    "url": "/about-me/static/css/main.5810b1bc.chunk.css"
+  },
+  {
+    "revision": "a3753372a587dd9ba7cb",
+    "url": "/about-me/static/js/2.3e0533b2.chunk.js"
+  },
+  {
+    "revision": "56a96feea4625ae615a6",
+    "url": "/about-me/static/js/main.fa2579f1.chunk.js"
+  },
+  {
+    "revision": "e174a773c375c70941f5",
+    "url": "/about-me/static/js/runtime-main.28d5be6e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/about-me/static/media/logo.25bf045c.svg"
+  }
+]);
